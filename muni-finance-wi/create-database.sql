7e58f5ccd7c853d5bfcfc67b371702c15ba69da2
Bill Jetzer <jetzerb@sva.com>
2025-03-30 19:04:07 -0500

    Add SQL script to create the tables & views

---
+++ muni-finance-wi/create-database.sql
@@ -0,0 +1,13 @@
+-- Create tables
+.read main/Tables/AWI.sql
+.read main/Tables/CPI.sql
+.read main/Tables/MuniAgi.sql
+.read main/Tables/MuniBudget.sql
+.read main/Tables/Municipality.sql
+.read main/Tables/MuniPop.sql
+.read main/Tables/SharedRevenue.sql
+
+-- Create views
+.read main/Views/AWI_View.sql
+.read main/Views/MuniBudget_View.sql
+.read main/Views/SharedRevenue_View.sql
